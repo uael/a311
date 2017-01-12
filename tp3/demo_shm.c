1507f86decba244034e2ca82ff8f84cc4abdfb5d
uael <lucas.abel@yandex.com>
2017-01-12 15:40:52 +0100

    ex1

--- tp3/demo_shm.c
+++ tp3/demo_shm.c
@@ -21,47 +21,59 @@
 #include <time.h>
 
 // Type de la zone mmoire
-typedef struct memoire
-  { int valeur;
-    int date;
-  } Memoire;
+typedef struct memoire {
+  int valeur;
+  int date;
+} Memoire;
 
-int main(int argc, char * argv[]) { 
-    struct memoire *zone; 
-    int memid, resultat;
+int main(int argc, char *argv[]) {
+  Memoire *zone;
+  int memid, resultat;
 
-    if (argc != 2) {fprintf (stderr, "\nSyntaxe : %s ( -creation | -use )\n\n", argv[0]); exit (1);}
-    if ((strcmp (argv[1], "-creation")!= 0) && (strcmp (argv[1], "-use")!= 0))
-                   {fprintf (stderr, "\nSyntaxe : %s ( -creation | -use )\n\n", argv[0]); exit (2);}
-    
-    /* cration ou lien avec une zone partage */
-    memid = shmget(CLEF, 100, 0700 | IPC_CREAT); 
-    if (memid == -1) { perror("shmget"); exit (-1); }
-    
-    /* attachement : montage en mmoire */
-    zone = shmat(memid, NULL, 0);
-    if (zone == (struct memoire*)-1) { perror("shmat"); exit (-1); }
-    
-    if (strcmp (argv[1], "-creation")== 0) 
-       { 
-         zone->valeur = 0; 
-         zone->date =time(NULL);
-         printf("\nCREATION : VALEURS en memoire : valeur = %d - date = %s\n",
-                zone->valeur, ctime ((time_t*)&zone->date));
-       }  
-      else // -use
-       { 
-         printf("\nUSE : ANCIENNES VALEURS en memoire : valeur = %d - date = %s \n",
-                zone->valeur, ctime ((time_t*)&zone->date));
-         zone->valeur++; 
-         zone->date =time(NULL);
-         printf("USE : NOUVELLES VALEURS en memoire : valeur = %d - date = %s \n",
-         zone->valeur, ctime ((time_t*)&zone->date));
-       }
+  if (argc != 2) {
+    fprintf(stderr, "\nSyntaxe : %s ( -creation | -use )\n\n", argv[0]);
+    exit(1);
+  }
+  if ((strcmp(argv[1], "-creation") != 0) && (strcmp(argv[1], "-use") != 0)) {
+    fprintf(stderr, "\nSyntaxe : %s ( -creation | -use )\n\n", argv[0]);
+    exit(2);
+  }
 
-    /* detachement */
-    resultat = shmdt (zone);
-    if (resultat == -1) { perror("shmdt"); exit (-1); }
-    printf("Memoire detachee\n\n");
+  /* cration ou lien avec une zone partage */
+  memid = shmget(CLEF, sizeof(Memoire), 0700 | IPC_CREAT);
+  if (memid == -1) {
+    perror("shmget");
+    exit(-1);
+  }
+
+  /* attachement : montage en mmoire */
+  zone = shmat(memid, NULL, 0);
+  if (zone == (Memoire *) -1) {
+    perror("shmat");
+    exit(-1);
+  }
+
+  if (strcmp(argv[1], "-creation") == 0) {
+    zone->valeur = 0;
+    zone->date = time(NULL);
+    printf("\nCREATION : VALEURS en memoire : valeur = %d - date = %s\n",
+           zone->valeur, ctime((time_t *) &zone->date));
+  } else // -use
+  {
+    printf("\nUSE : ANCIENNES VALEURS en memoire : valeur = %d - date = %s \n",
+           zone->valeur, ctime((time_t *) &zone->date));
+    zone->valeur++;
+    zone->date = time(NULL);
+    printf("USE : NOUVELLES VALEURS en memoire : valeur = %d - date = %s \n",
+           zone->valeur, ctime((time_t *) &zone->date));
+  }
+
+  /* detachement */
+  resultat = shmdt(zone);
+  if (resultat == -1) {
+    perror("shmdt");
+    exit(-1);
+  }
+  printf("Memoire detachee\n\n");
 }
 
---
+++ tp3/shm2.c
@@ -0,0 +1,44 @@
+/******************************************************************/
+/* IUT NICE-COTE D'AZUR - Departement INFORMATIQUE - R. CHIGNOLI  */
+/* Module DUT SYS-RX                              Theme 1 IPC S5  */
+/******************************************************************/
+/* shm2.c : Squelette pour les questions 2.X                      */
+/******************************************************************/
+#include <stdlib.h> 
+#include <stdio.h>
+#include <sys/ipc.h> 
+#include <sys/shm.h>
+
+typedef struct _shared_memory_t {
+  int count;
+} shared_memory_t;
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    fprintf(stderr, "\nSyntaxe : %s num-cle\n\n", argv[0]);
+    exit(1);
+  }
+  int memid, result, key = atoi(argv[1]);
+  shared_memory_t *memory;
+
+  memid = shmget(key, sizeof(shared_memory_t), 0777 | IPC_CREAT);
+  if (memid == -1) {
+    perror("shmget");
+    exit(-1);
+  }
+
+  memory = shmat(memid, NULL, 0);
+  if (memory == (shared_memory_t *) -1) {
+    perror("shmat");
+    exit(-1);
+  }
+
+  printf("%d", ++memory->count);
+
+  result = shmdt(memory);
+  if (result == -1) {
+    perror("shmdt");
+    exit(-1);
+  }
+}
+
